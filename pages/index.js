1320ec0ffb39638453d34defa63852c3c70a03be
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-06-30 14:24:56 -0300

    Front-end react-nextjs

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,11 @@ import {
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fas);
+
 function HomePage() {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -52,7 +57,7 @@ function HomePage() {
                     padding-top: 150px;
                     padding-bottom: 100px;
                     margin-bottom: 0rem !important;
-                }`
+                    }`
                 }</style>
                 <Container className="text-center">
                     <h1 className="display-4 mb-4">Temos a solução que sua empresa precisa!</h1>
@@ -70,26 +75,42 @@ function HomePage() {
                    padding-top: 80px;
                    padding-bottom: 80px;
                    margin-bottom: 0rem !important;
-               }`
+                    }.circulo{
+                   width: 140px;
+                   height: 140px;
+                   background-color: #fed136;
+                   font-size: 52px;
+                   padding-top: 24px;
+                   color: #fff;
+                    }.centralizar{
+                   margin: 0 auto !important;
+                   float: none !important;
+                    }`
                 }</style>
                 <Container className="text-center">
                     <div>
                         <h2 className="display-4">Serviços</h2>
                         <p className="lead pb-4">Featured content or information</p>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="rounded-circle circulo centralizar">
+                            <FontAwesomeIcon icon="laptop-code"/>
+                            </div>
                             <h2 className="mt-4 mb-4">Serviço 1</h2>
                             <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
                         </div>
-                        <div class="col-lg-4">
-                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                        <div className="col-lg-4">
+                            <div className="rounded-circle circulo centralizar">
+                            <FontAwesomeIcon icon="mobile-alt"/>
+                            </div>
                             <h2 className="mt-4 mb-4">Serviço 2</h2>
                             <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
                         </div>
-                        <div class="col-lg-4">
-                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                        <div className="col-lg-4">
+                            <div className="rounded-circle circulo centralizar">
+                            <FontAwesomeIcon icon="bullhorn"/>
+                            </div>
                             <h2 className="mt-4 mb-4">Serviço 3</h2>
                             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                         </div>
